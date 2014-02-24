481e3ba9c1b8e85337a59a123cf695ae21af9f95
Johannes Gorset <jgorset@gmail.com>
2014-02-24 13:26:12 +0100

    Remove feed items

--- app/models/mingle/feed/item.rb
+++
@@ -1,8 +0,0 @@
-class Mingle::Feed::Item < ActiveRecord::Base
-  belongs_to :feedable, polymorphic: true, inverse_of: :feed_items
-
-  validates :feedable, presence: true
-
-  default_scope { order('sticky DESC').order('created_at DESC') }
-  scope :published, lambda { where(published: true) }
-end
--- spec/models/mingle/feed/item_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-describe Mingle::Feed::Item do
-end
