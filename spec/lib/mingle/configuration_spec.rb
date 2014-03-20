7583ec1b72aa92ca3cb0dafc34de46fadad6849f
Johannes Gorset <jgorset@gmail.com>
2014-03-20 23:53:18 +0100

    Make specs idempotent

--- spec/lib/mingle/configuration_spec.rb
+++ spec/lib/mingle/configuration_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
 
 describe Mingle::Configuration do
-  {
+  around do |example|
+    Mingle.temporarily &example
+  end
+
+  after :all do
+    ENVIRONMENT_ENABLED_CONFIGURATIONS.each { |key, value| ENV[key.to_s.upcase] = nil }
+  end
+
+  ENVIRONMENT_ENABLED_CONFIGURATIONS = {
     facebook_access_token: 'FACEBOOK_ACCESS_TOKEN',
     twitter_api_key: 'TWITTER_API_KEY',
     twitter_api_secret: 'TWITTER_API_SECRET',
     twitter_access_token: 'TWITTER_ACCESS_TOKEN',
     twitter_access_token_secret: 'TWITTER_ACCESS_TOKEN_SECRET',
-    instagram_client_id: 'INSTAGRAM_CLIENT_ID'
-  }.each do |key, value|
-    before { ENV[value] = 'some-environment-secret' }
+    instagram_client_id: 'INSTAGRAM_CLIENT_ID',
+  }
 
+  NON_ENVIRONMENT_ENABLED_CONFIGURATIONS = {
+    since: 2.days.ago
+  }
+
+  CONFIGURATIONS = ENVIRONMENT_ENABLED_CONFIGURATIONS.merge NON_ENVIRONMENT_ENABLED_CONFIGURATIONS
+
+  CONFIGURATIONS.each do |key, value|
     it "responds to #{key}=" do
       expect(subject).to respond_to "#{key}=".to_sym
     end
@@ -21,10 +35,12 @@ describe Mingle::Configuration do
 
     describe '##{key}' do
       context 'when it is not configured through Mingle.configure' do
+        before { ENV[key.to_s.upcase] = 'some-environment-secret' }
+
         it "fetches the configuration from the #{value} environment variable" do
           expect(subject.send(key)).to eq 'some-environment-secret'
         end
-      end
+      end unless key.in? NON_ENVIRONMENT_ENABLED_CONFIGURATIONS
 
       context 'when it has been configured through Mingle' do
         before do
--- spec/lib/mingle_spec.rb
+++ spec/lib/mingle_spec.rb
@@ -2,16 +2,14 @@ require 'spec_helper'
 
 describe Mingle do
   describe '.temporarily' do
-    before do
-      Mingle.configure { |config| config.facebook_access_token = 'original access token' }
-    end
-
     it 'sets temporary configuration options' do
+      expect(Mingle.config.facebook_access_token).to be_nil
+
       Mingle.temporarily facebook_access_token: 'new access token' do
         expect(Mingle.config.facebook_access_token).to eq 'new access token'
       end
 
-      expect(Mingle.config.facebook_access_token).to eq 'original access token'
+      expect(Mingle.config.facebook_access_token).to be_nil
     end
   end
 end
