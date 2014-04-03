4059d91d699c22254d385541bc5bc14d5de4fa25
Elvis Nunez <elvisnunez@me.com>
2014-04-03 10:50:06 +0200

    Add tweet url to Mingle::Twitter::Tweet

--- spec/models/mingle/twitter/tweet_spec.rb
+++ spec/models/mingle/twitter/tweet_spec.rb
@@ -12,4 +12,12 @@ describe Mingle::Twitter::Tweet do
       expect(subject.avatar).to eq subject.user_image_url
     end
   end
+  
+  describe "#url" do
+    it 'should be the tweet url' do
+      subject.stub(:user_handle).and_return 'sindre'
+      subject.stub(:tweet_id).and_return '1337'
+      expect(subject.url).to eq "https://twitter.com/sindre/status/1337"
+    end
+  end
 end
